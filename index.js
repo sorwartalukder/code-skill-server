d9e047031c00a2689007c614da026650a1f2591b
sorwartalukder <mdsorwar4039@gmail.com>
2022-10-26 02:23:37 +0600

    send courses data

--- index.js
+++ index.js
@@ -2,6 +2,12 @@ const express = require('express')
 const app = express()
 const port = process.env.PORT || 5000;
 
+
+const courses = require('./data/courses.json');
+
+app.get('/courses', (req, res) => {
+    res.send(courses)
+})
 app.get('/', (req, res) => {
     res.send('Hello World!')
 })
